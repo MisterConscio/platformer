df672700d8cb873571325791d36ea7a5343011e0
MisterConscio <saulo.costam@gmail.com>
2023-07-21 16:59:18 -0300

    Scaling display resolution

--- game.py
+++ game.py
@@ -10,39 +10,42 @@ class Game:
         pygame.display.set_caption("Ninja")
 
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+
         self.clock = pygame.time.Clock()
 
         self.movement = [False, False]
 
         self.assets = {
-                'player': load_image('entities/player.png')
-                }
+            'player': load_image('entities/player.png')
+        }
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
 
             self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_h:
                         self.movement[0] = True
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_l:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_h:
                         self.movement[0] = False
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_l:
                         self.movement[1] = False
 
 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
